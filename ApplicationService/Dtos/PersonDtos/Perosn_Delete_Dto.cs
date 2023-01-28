e4a0e51d7c603c46864765267f696abfb4818b6f
mohammad-rezaei <mohammad.rezaei2434@gmail.com>
2023-01-29 02:33:55 +0330

    delete

--- ApplicationService/Dtos/PersonDtos/Perosn_Delete_Dto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApplicationService.Dtos.PersonDtos
-{
-    public class Perosn_Delete_Dto
-    {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-    }
-}
--- ApplicationService/Dtos/PersonDtos/Person_Detail_Dto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApplicationService.Dtos.PersonDtos
-{
-    public class Person_Detail_Dto
-    {
-        public Guid Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-    }
-}
